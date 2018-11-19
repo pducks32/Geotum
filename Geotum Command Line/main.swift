c1e03972369183260f2ef97b795c7b1acc6454af
Patrick Metcalfe <git@patrickmetcalfe.com>
2018-11-19 15:42:22 -0600

    Add Geotum Command Line

---
+++ Geotum Command Line/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Geotum Command Line
+//
+//  Created by Patrick Metcalfe on 11/19/18.
+//
+
+import Foundation
+
+
+
--- Package.swift
+++ Package.swift
@@ -10,10 +10,14 @@ let package = Package(
         .library(
             name: "Geotum",
             targets: ["Geotum"]),
+        .executable(
+            name: "geotum",
+            targets: ["Geotum Command Line"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,5 +28,10 @@ let package = Package(
         .testTarget(
             name: "GeotumTests",
             dependencies: ["Geotum"]),
+        .target(
+            name: "Geotum Command Line",
+            dependencies: ["Geotum", "Utility"],
+            path: "Geotum Command Line"
+        )
     ]
 )
